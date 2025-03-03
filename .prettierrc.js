d1de033db9faa7c3b8983b3b2e8a39b8df1166d1
Soy <soyeon.won@oxbridgeuk.com>
2025-03-03 00:44:15 +0000

    set Eslint, Prettier :

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true, // 문자열을 작은따옴표(')로 사용
+  trailingComma: "all", // 객체, 배열 등에서 마지막 요소 뒤에도 쉼표 추가
+  tabWidth: 2, // 탭 너비 설정 (기본값: 2)
+  semi: true, // 문장 끝에 세미콜론(;) 추가
+  useTabs: false, // 들여쓰기에 탭 대신 공백 사용
+  printWidth: 100, // 한 줄 최대 길이 (기본값: 80)
+  arrowParens: "always", // 화살표 함수의 매개변수가 하나일 때도 괄호 사용
+  bracketSpacing: true, // 객체 리터럴에서 중괄호 안에 공백 추가
+  jsxSingleQuote: false, // JSX에서 큰따옴표(") 사용 (기본값: false)
+  proseWrap: "preserve", // 마크다운 텍스트 줄바꿈 스타일 (기본값: 'preserve')
+  endOfLine: "lf", // 줄바꿈 스타일 (LF로 설정)
+};
